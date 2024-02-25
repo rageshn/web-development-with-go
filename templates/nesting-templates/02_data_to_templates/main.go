cea7ce641a9f68789563099d8aa8f7437bd17015
Ragesh <ragesh.narayanan@gmail.com>
2024-02-25 21:12:42 +0530

    added nesting templates

---
+++ templates/nesting-templates/02_data_to_templates/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", 50)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+}
